61be55039aa27dbbd2714ff6cdd0965f20d667a4
Finn Bledsoe <89262886+bledsoef@users.noreply.github.com>
2024-09-24 01:44:53 +0000

    added answers and started writing memory-user.c

---
+++ memory-user.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    int megs = argv[1];
+    int* arrayPtr;
+    arrayPtr = (int *)malloc(megs);
+
+    while (1) {
+        arrayPtr
+    }
+
+    free(arrayPtr);
+}
