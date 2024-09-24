747fad02945bb49004481338ab0bd90e4da49a4f
Finn Bledsoe <89262886+bledsoef@users.noreply.github.com>
2024-09-24 11:44:14 +0000

    completed quesiton 7

--- memory-user.c
+++ memory-user.c
@@ -17,7 +17,6 @@ int main(int argc, char *argv[]) {
     int *startPtr = arrayPtr;
 
     while (1) {
-        printf("%d ", *arrayPtr);
         arrayPtr++; 
 
         if (arrayPtr == startPtr + arraySize) {
