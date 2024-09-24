db2f44b508b3db2f3ed5833f72214e0b6947a938
Finn Bledsoe <89262886+bledsoef@users.noreply.github.com>
2024-09-24 01:58:04 +0000

    completed question 3

--- memory-user.c
+++ memory-user.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char *argv[]) {
-    int megs = argv[1];
-    int* arrayPtr;
-    arrayPtr = (int *)malloc(megs);
 
-    while (1) {
-        arrayPtr
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <bytes>\n", argv[0]);
+        return 1;
     }
 
-    free(arrayPtr);
+    int bytes = atoi(argv[1]);
+    int *arrayPtr = (int *)malloc(bytes);
+    if (arrayPtr == NULL) {
+        return 1;
+    }
+
+    int arraySize = (bytes) / sizeof(int); 
+    int *startPtr = arrayPtr;
+
+    while (1) {
+        printf("%d ", *arrayPtr);
+        arrayPtr++; 
+
+        if (arrayPtr == startPtr + arraySize) {
+            arrayPtr = startPtr;
+        }
+    }
+
+    free(startPtr);
+    return 0;
 }
